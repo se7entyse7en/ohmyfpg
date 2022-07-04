bce4c73356a158a66c6229caabf1d3bcda97cb38
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2022-07-04 19:46:52 +0200

    Add `messages/startup` module
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

---
+++ src/messages/startup/tests.rs
@@ -0,0 +1,16 @@
+use crate::messages::StartupMessage;
+
+#[test]
+fn test_serialize() {
+    let params = vec![("user".to_owned(), "postgres".to_owned())];
+    let m = StartupMessage::new(params.to_owned());
+    assert_eq!(m.version, (3, 0));
+    assert_eq!(m.params, params);
+    assert_eq!(
+        m.serialize(),
+        vec![
+            0, 0, 0, 23, 0, 3, 0, 0, 117, 115, 101, 114, 0, 112, 111, 115, 116, 103, 114, 101, 115,
+            0, 0
+        ]
+    );
+}
