32cf33c4f014da6b3f3e432a479d301ea8643a3e
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2022-07-05 22:03:48 +0200

    Refactor how python bindings are exposed
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

---
+++ src/bindings.rs
@@ -0,0 +1,37 @@
+use crate::client::{self, Connection, ConnectionError};
+use futures::future::FutureExt;
+use pyo3::create_exception;
+use pyo3::exceptions::{PyException, PyOSError};
+use pyo3::prelude::*;
+
+create_exception!(ohmyfpg, PyInvalidDSNError, PyException, "Invalid DSN.");
+
+/// Connect to the database and return a `Connection` object.
+#[pyfunction]
+pub fn connect(py: Python, dsn: String) -> PyResult<&PyAny> {
+    pyo3_asyncio::tokio::future_into_py(
+        py,
+        client::connect(dsn).map(|res| res.map(PyConnection::from).map_err(PyErr::from)),
+    )
+}
+
+/// Connection object exposed to Python
+#[pyclass(name = "Connection")]
+pub struct PyConnection {
+    _wrappee: Connection,
+}
+
+impl From<Connection> for PyConnection {
+    fn from(conn: Connection) -> Self {
+        PyConnection { _wrappee: conn }
+    }
+}
+
+impl From<ConnectionError> for PyErr {
+    fn from(err: ConnectionError) -> Self {
+        match err {
+            ConnectionError::InvalidDSNError(err) => PyInvalidDSNError::new_err(err.to_string()),
+            ConnectionError::IOError(err) => PyOSError::new_err(err.to_string()),
+        }
+    }
+}
