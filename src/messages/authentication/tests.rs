695d94456720610c46dd05662d95fea55f34a05f
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2022-07-09 12:17:21 +0200

    Implement `DeserializeMessage` for `AuthenticationSASL`
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- src/messages/authentication/tests.rs
+++ src/messages/authentication/tests.rs
@@ -1,13 +1,14 @@
-use crate::messages::{AuthenticationSASL, SerializeMessage};
+use crate::messages::{AuthenticationSASL, DeserializeMessage, SerializeMessage};
 
 #[test]
-fn test_serialize() {
-    let m = AuthenticationSASL::new(vec!["SCRAM-SHA-256".to_owned()]);
-    assert_eq!(m.mechanisms, vec!["SCRAM-SHA-256".to_owned()]);
-    assert_eq!(
-        m.serialize(),
-        vec![
-            82, 0, 0, 0, 23, 0, 0, 0, 10, 83, 67, 82, 65, 77, 45, 83, 72, 65, 45, 50, 53, 54, 0, 0
-        ]
-    );
+fn test_serialize_deserialize() {
+    let m1 = AuthenticationSASL::new(vec!["SCRAM-SHA-256".to_owned()]);
+    let bytes = vec![
+        82, 0, 0, 0, 23, 0, 0, 0, 10, 83, 67, 82, 65, 77, 45, 83, 72, 65, 45, 50, 53, 54, 0, 0,
+    ];
+    assert_eq!(m1.mechanisms, vec!["SCRAM-SHA-256".to_owned()]);
+    assert_eq!(m1.serialize(), bytes);
+
+    let m2 = AuthenticationSASL::deserialize_body(bytes[5..].to_vec());
+    assert_eq!(m2.mechanisms, vec!["SCRAM-SHA-256".to_owned()]);
 }
