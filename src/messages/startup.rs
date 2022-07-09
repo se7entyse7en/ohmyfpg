32d03ae2c9268a8ca921229f034c610d8136d891
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2022-07-09 12:52:52 +0200

    Implement `DeserializeMessage` for `StartupMessage`
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- src/messages/startup.rs
+++ src/messages/startup.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod tests;
-
+#[cfg(test)]
+use crate::messages::DeserializeMessage;
 use crate::messages::{SerializeMessage, SerializeMessageBytes};
 
 static PROTOCOL_MAJOR_VALUE: u16 = 3;
@@ -19,6 +20,11 @@ impl StartupMessage {
             params,
         }
     }
+
+    #[cfg(test)]
+    pub fn new_with_version(version: (u16, u16), params: Vec<(String, String)>) -> Self {
+        StartupMessage { version, params }
+    }
 }
 
 impl SerializeMessage for StartupMessage {
@@ -35,5 +41,34 @@ impl SerializeMessage for StartupMessage {
         params.push(0x00);
         body.append(&mut params);
         body
+    }
+}
+
+#[cfg(test)]
+impl DeserializeMessage for StartupMessage {
+    fn deserialize_body(body: Vec<u8>) -> Self {
+        let version_raw: ([u8; 2], [u8; 2]) = (
+            body[4..6].try_into().unwrap(),
+            body[6..8].try_into().unwrap(),
+        );
+        let version = (
+            u16::from_be_bytes(version_raw.0),
+            u16::from_be_bytes(version_raw.1),
+        );
+        let mut params = Vec::new();
+        let clean_iter = body[8..]
+            .split(|b| *b == 0)
+            .filter(|chunk| !chunk.is_empty());
+        let iter_even = clean_iter.to_owned().step_by(2);
+        let iter_odd = clean_iter.skip(1).step_by(2);
+        let iter = iter_even.zip(iter_odd);
+        for (key, value) in iter {
+            params.push((
+                String::from_utf8(key.to_vec()).unwrap(),
+                String::from_utf8(value.to_vec()).unwrap(),
+            ));
+        }
+
+        StartupMessage::new_with_version(version, params)
     }
 }
--- src/messages/startup/tests.rs
+++ src/messages/startup/tests.rs
@@ -1,16 +1,18 @@
-use crate::messages::{SerializeMessage, StartupMessage};
+use crate::messages::{DeserializeMessage, SerializeMessage, StartupMessage};
 
 #[test]
 fn test_serialize() {
     let params = vec![("user".to_owned(), "postgres".to_owned())];
-    let m = StartupMessage::new(params.to_owned());
-    assert_eq!(m.version, (3, 0));
-    assert_eq!(m.params, params);
-    assert_eq!(
-        m.serialize(),
-        vec![
-            0, 0, 0, 23, 0, 3, 0, 0, 117, 115, 101, 114, 0, 112, 111, 115, 116, 103, 114, 101, 115,
-            0, 0
-        ]
-    );
+    let m1 = StartupMessage::new(params.to_owned());
+    let bytes = vec![
+        0, 0, 0, 23, 0, 3, 0, 0, 117, 115, 101, 114, 0, 112, 111, 115, 116, 103, 114, 101, 115, 0,
+        0,
+    ];
+    assert_eq!(m1.version, (3, 0));
+    assert_eq!(m1.params, params);
+    assert_eq!(m1.serialize(), bytes);
+
+    let m2 = StartupMessage::deserialize_body(bytes);
+    assert_eq!(m2.version, (3, 0));
+    assert_eq!(m2.params, params);
 }
