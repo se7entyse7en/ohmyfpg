afefcf6a87d7e17734ff76adc9389c879b6fece3
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2022-09-29 21:15:43 +0200

    Run performance comparison within docker
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- performance/compare.py
+++ performance/compare.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 TEST_RUNS = 30
 HEAT_RUNS = 3
-DSN = 'postgres://postgres:postgres@localhost:5432/postgres'
+DSN = 'postgres://postgres:postgres@postgres:5432/postgres'
 QUERY = 'SELECT * FROM performance_test'
 
 
