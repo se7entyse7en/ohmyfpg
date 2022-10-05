971b6a4794952dc1018a4da2fa165cd81432370e
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2022-10-05 22:46:32 +0200

    Add `Parse` message in `messages::query::frontend`
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- ohmyfpg_core/src/messages/query.rs
+++ ohmyfpg_core/src/messages/query.rs
@@ -1,5 +1,5 @@
 mod frontend;
-pub use frontend::Query;
+pub use frontend::{Parse, Query};
 mod backend;
 pub use backend::{
     CommandComplete, DataRow, RowDescription, COMMAND_COMPLETE_MESSAGE_TYPE, DATA_ROW_MESSAGE_TYPE,
--- ohmyfpg_core/src/messages/query/frontend.rs
+++ ohmyfpg_core/src/messages/query/frontend.rs
@@ -1,6 +1,7 @@
 use crate::messages::{SerializeMessage, SerializeMessageBytes};
 
 const QUERY_MESSAGE_TYPE: &[u8; 1] = b"Q";
+const PARSE_MESSAGE_TYPE: &[u8; 1] = b"P";
 
 #[derive(Debug)]
 pub struct Query {
@@ -20,5 +21,31 @@ impl SerializeMessage for Query {
 
     fn serialize_body(self) -> Vec<u8> {
         self.query_string.to_msg_bytes()
+    }
+}
+
+#[derive(Debug)]
+pub struct Parse {
+    pub query_string: String,
+}
+
+impl Parse {
+    pub fn new(query_string: String) -> Self {
+        Parse { query_string }
+    }
+}
+
+impl SerializeMessage for Parse {
+    fn get_msg_type(&self) -> Option<&[u8; 1]> {
+        Some(PARSE_MESSAGE_TYPE)
+    }
+
+    fn serialize_body(self) -> Vec<u8> {
+        let mut body = vec![];
+        let mut prepared_stmt_name = "".to_string().to_msg_bytes();
+        body.append(&mut prepared_stmt_name);
+        body.append(&mut self.query_string.to_msg_bytes());
+        body.append(&mut 0u16.to_msg_bytes());
+        body
     }
 }
